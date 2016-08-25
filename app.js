0aa369c6f23967636b848b0df571f20badc7770b
anniekbh <anniekbh@gmail.com>
2016-08-25 14:53:11 -0500

    subiendo archivos

---
+++ app.js
@@ -0,0 +1,10 @@
+var numero1;  
+var numero2;
+var suma;
+         
+numero1=prompt("Ingrese el primer numero : ","");
+numero2=prompt("Ingrese el segundo numero : ","");
+suma= (parseInt(numero1)+ parseInt(numero2));
+
+window.alert("El resultado de la suma es : " + suma);
+	
